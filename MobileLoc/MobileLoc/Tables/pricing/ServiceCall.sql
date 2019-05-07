952c494ced090de93e9fed6574cae8f7ace41dc6
JHBlackburn <JHBlackburn@gmail.com>
2019-05-07 12:38:07 -0400

    added service call pricing table, cleaned up job location table

---
+++ MobileLoc/MobileLoc/Tables/pricing/ServiceCall.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [pricing].[ServiceCall]
+(
+	ServiceCallId					INT					NOT NULL	IDENTITY(1,1),
+	ServiceCallZipCodeShort			INT					NOT NULL,
+	ServiceCallPrice				DECIMAL(18,2)		NOT NULL,
+	InsertDT						DATETIME			NOT NULL	DEFAULT(GETDATE()),
+	InsertBy						INT					NOT NULL	DEFAULT(0),
+	UpdateDT						DATETIME			NOT NULL	DEFAULT(GETDATE()),
+	UpdateBy						INT					NOT NULL	DEFAULT(0),
+	ChangeVersion					ROWVERSION,
+	
+		CONSTRAINT PC_pricing_ServiceCall PRIMARY KEY CLUSTERED (ServiceCallId asc)
+		WITH (DATA_COMPRESSION = PAGE)
+)
