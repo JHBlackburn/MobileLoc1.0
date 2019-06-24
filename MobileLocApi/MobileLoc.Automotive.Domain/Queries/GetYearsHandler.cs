7a717b9601664ed6725037d695aa1dfb360e4a34
JHBlackburn <JHBlackburn@gmail.com>
2019-06-23 22:57:16 -0400

    handler for GetYears

---
+++ MobileLocApi/MobileLoc.Automotive.Domain/Queries/GetYearsHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using MobileLoc.Automotive.Domain.Dtos;
+using MobileLoc.Automotive.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MobileLoc.Automotive.Domain.Queries
+{
+    public class GetYearsHandler : IRequestHandler<GetYears, IEnumerable<GetYearsDto>>
+    {
+        private readonly ILookupRepository _lookupRepository;
+
+        public GetYearsHandler(ILookupRepository lookupRepository)
+        {
+            _lookupRepository = lookupRepository;
+        }
+
+        public async Task<IEnumerable<GetYearsDto>> Handle(GetYears request, CancellationToken cancellationToken)
+        {
+            return await _lookupRepository.GetActiveYearsByModelAsync(request.ModelId);
+        }
+    }
+}
--- MobileLocApi/MobileLoc.Automotive.Test/Domain/Queries/GetYearsTests.cs
+++ MobileLocApi/MobileLoc.Automotive.Test/Domain/Queries/GetYearsTests.cs
@@ -1,5 +1,11 @@
-﻿using MobileLoc.Automotive.Domain.Queries;
+﻿using FluentAssertions;
+using MobileLoc.Automotive.Domain.Dtos;
+using MobileLoc.Automotive.Domain.Interfaces;
+using MobileLoc.Automotive.Domain.Queries;
+using Moq;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -7,6 +13,33 @@ namespace MobileLoc.Automotive.Test.Domain.Queries
 {
     public class GetYearsTests
     {
+        [Fact]
+        public async Task Handle_GivenModelId_ReturnsExpectedYears()
+        {
+            // arrange
+            var modelId = 132;
+            var request = new GetYears(modelId);
+            var lookupRepo = new Mock<ILookupRepository>();
+            var handler = new GetYearsHandler(lookupRepo.Object);
+            var expectedResults = new List<GetYearsDto>
+            {
+                new GetYearsDto
+                {
+                    CarYearId = 1,
+                    CarYear = "1901",
+                    CarModelId = 5,
+                    IsActive = true,
+                },
+            };
+            lookupRepo.Setup(repo => repo.GetActiveYearsByModelAsync(It.IsAny<int>())).ReturnsAsync(expectedResults);
+
+            // act
+            var results = await handler.Handle(request, new CancellationToken());
+
+            //assert
+            results.Should().BeEquivalentTo(expectedResults);
+        }
+
         [Theory]
         [InlineData(0)]
         [InlineData(-1)]
