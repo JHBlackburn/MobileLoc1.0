829ab064a398d34620644a9d7ea61173f8dac984
JHBlackburn <JHBlackburn@gmail.com>
2019-06-23 22:49:10 -0400

    Test and Request for GetYears

---
+++ MobileLocApi/MobileLoc.Automotive.Domain/Queries/GetYears.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using MobileLoc.Automotive.Domain.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileLoc.Automotive.Domain.Queries
+{
+    public class GetYears : IRequest<IEnumerable<GetYearsDto>>
+    {
+        public int ModelId;
+
+        public GetYears(int modelId)
+        {
+            ModelId = modelId;
+
+            Validate();
+        }
+
+        private void Validate()
+        {
+            var errorsList = new StringBuilder();
+
+            if (ModelId <= 0)
+            {
+                errorsList.Append($"Invalid value provided for {nameof(ModelId)}");
+            }
+
+            if (errorsList.Length > 0)
+            {
+                throw new ArgumentException(errorsList.ToString());
+            }
+        }
+    }
+}
---
+++ MobileLocApi/MobileLoc.Automotive.Test/Domain/Queries/GetYearsTests.cs
@@ -0,0 +1,19 @@
+﻿using MobileLoc.Automotive.Domain.Queries;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MobileLoc.Automotive.Test.Domain.Queries
+{
+    public class GetYearsTests
+    {
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        public async Task Constructor_GivenNoModelId_ThrowsArgumentException(int initializedModelId)
+        {
+            // act and assert
+            var exception = Assert.Throws<ArgumentException>(() => new GetYears(initializedModelId));
+        }
+    }
+}
