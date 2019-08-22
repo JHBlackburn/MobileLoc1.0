c65090680c5beb6cb9cb8f413258f7204e51fe92
John Blackburn <jblackburn@onecallcm.com>
2019-08-22 19:20:00 -0400

    Can read a cell out of a hardcoded file path and cell location. Baby steps.

---
+++ MobileLocApi/MobileLoc.Automotive.Persistence/Repositories/DataScrapeRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileLoc.Automotive.Persistence.Repositories
+{
+    public class DataScrapeRepository
+    {
+        public string SpreadsheetTest()
+        {
+            // Open MyData.xls.
+            SpreadsheetGear.IWorkbook workbook = SpreadsheetGear.Factory.GetWorkbook(@"C:\git\_DELETEME\MobileLoc1.0\MobileLocApi\Files\2019-auto-truck-key-blank-reference.xlsx");
+            // Get a reference to the first worksheet.
+            SpreadsheetGear.IWorksheet worksheet = workbook.Worksheets["Page 3"];
+            // Get a reference to the top left cell of Sheet1.
+            SpreadsheetGear.IRange a16 = worksheet.Cells["A16"];
+            // Output the value of A1.
+            var message = a16.Value.ToString();
+
+            return message;
+        }
+    }
+}
---
+++ MobileLocApi/MobileLoc.Automotive.Test/Persistence/Repositories/DataScrapeRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using MobileLoc.Automotive.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MobileLoc.Automotive.Test.Persistence.Repositories
+{
+    public class DataScrapeRepositoryTests
+    {
+        [Fact]
+        public void SpreadsheetTest_GivenHardcodedStuff_ReturnsValueOfCell()
+        {
+            var dataScrapeRepo = new DataScrapeRepository();
+
+            dataScrapeRepo.SpreadsheetTest();
+        }
+    }
+}
